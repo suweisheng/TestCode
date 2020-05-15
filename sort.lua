e647ba2519d013cc0a2a722a96b58b805f47d019
suweisheng <1273469574@qq.com>
2020-05-15 11:24:51 +0800

    add sort.lua

---
+++ sort.lua
@@ -0,0 +1,154 @@
+local M = {}
+
+-- some function tools
+local function print_list(list)
+    local str = "["
+    for k, v in ipairs(list) do
+        str = str..v..", "
+    end
+    str = str.."]"
+    print(str)
+end
+
+local function copy(tb)
+    local copy_tb = {}
+    for k, v in pairs(tb) do
+        copy_tb[k] = v
+    end
+    return copy_tb
+end
+
+local raw_math_random = math.random
+math.random = function(a, b)
+    math.randomseed(os.time())
+    return raw_math_random(a, b)
+end
+
+local function shuffle(tb)
+    local j
+    for i=#tb, 2, -1 do
+        j = math.random(1, i)
+        tb[i], tb[j] = tb[j], tb[i]
+    end
+    return tb
+end
+
+-- test list
+local test_list = {0,9,8,7,6,5,4,3,2,1,2,-4,-8,-3}
+local test_list = {}
+for i=1, 15 do
+    test_list[#test_list+1] = i
+end
+shuffle(test_list)
+print_list(test_list)
+
+function M.bubble(list)
+    for i=1, #list-1 do
+        local is_swap = false
+        for j=1, #list-i do
+            if list[j+1] < list[j] then
+                list[j], list[j+1] = list[j+1], list[j]
+                is_swap = is_swap or true
+            end
+        end
+        if not is_swap then break end
+    end
+    return list
+end
+print_list(M.bubble(copy(test_list)))
+
+function M.choise(list)
+    for i=1, #list-1 do
+        local min_index = i
+        for j=i+1, #list do
+            if list[min_index] > list[j] then
+                min_index = j
+            end
+        end
+        list[i], list[min_index] = list[min_index], list[i]
+    end
+    return list
+end
+print_list(M.choise(copy(test_list)))
+
+function M.insert(list)
+    for i=2, #list do
+        local min_value = list[i]
+        for j=i, 2, -1 do
+            if min_value < list[j-1] then
+                list[j] = list[j-1]
+                i = j - 1
+            end
+        end
+        list[i] = min_value
+    end
+    return list
+end
+print_list(M.insert(copy(test_list)))
+
+function M.quick(list)
+    local function __quick(_list, left, right)
+        if left >= right then return end
+        local base = _list[left]
+        local i, j = left, right
+        while i < j do
+            -- j must be forward begor i
+            while i < j and _list[j] > base do
+                j = j - 1
+            end
+            while i < j and _list[i] <= base do
+                i = i + 1
+            end
+            if i < j then
+                _list[i], _list[j] = _list[j], _list[i]
+            end
+        end
+        _list[i], _list[left] = _list[left], _list[i]
+        __quick(_list, left, i-1)
+        __quick(_list, i+1, right)
+    end
+    __quick(list, 1, #list)
+    return list
+end
+print_list(M.quick(copy(test_list)))
+
+function M.merge(list)
+    local function __merge(left, mid, right)
+        local i, j  = left, mid+1
+        local tmp, k = {}, 1
+        while i <= mid and j <= right do
+            if list[i] < list[j] then
+                tmp[k] = list[i]
+                i = i + 1
+            else
+                tmp[k] = list[j]
+                j = j + 1
+            end
+            k = k + 1
+        end
+        while i <= mid do
+            tmp[k] = list[i] 
+            k = k + 1
+            i = i + 1
+        end
+        while j <= right do
+            tmp[k] = list[j] 
+            k = k + 1
+            j = j + 1
+        end
+        for i=1, #tmp do
+            list[left+i-1] = tmp[i]
+        end
+    end
+    local function __separate(left, right)
+        if left >= right then return end
+        local mid =  math.floor((left+right)/2)
+        __separate(left, mid)
+        __separate(mid+1, right)
+        __merge(left, mid, right)
+    end
+    __separate(1, #list)
+    return list
+end
+print_list(M.merge(copy(test_list)))
+

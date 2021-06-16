f83f4e38783bee29e9341ff9da0bcdae5ac00d6a
suweisheng <1273469574@qq.com>
2021-06-16 11:36:07 +0800

    new question

--- pyleet.py
+++ pyleet.py
@@ -496,7 +496,6 @@ class Solution(object):
 # ret = Solution().myPow(x, n)
 # print ret
 
-
 def mergeKLists(self, lists):
     """
     :type lists: List[ListNode]
@@ -725,4 +724,43 @@ class Solution(object):
 # dividend = 7
 # divisor = -3
 # ret = Solution().divide(dividend, divisor)
+# print ret
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        # init data
+        rows = [{} for i in xrange(9)]
+        columns = [{} for i in xrange(9)]
+        boxes = [{} for i in xrange(9)]
+        for i in xrange(9):
+            for j in xrange(9):
+                num = board[i][j]
+                if num == '.': continue
+                box_index = (i // 3) * 3 + j // 3
+                # keep the current cell value
+                rows[i][num] = rows[i].get(num, 0) + 1
+                columns[j][num] = columns[j].get(num, 0) + 1
+                boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
+                # check if this value has been already seen before
+                if (rows[i][num] > 1
+                    or columns[j][num] > 1
+                    or boxes[box_index][num] > 1):
+                    return False
+        return True
+# board = [
+#     ["5","5",".",".","7",".",".",".","."],
+#     ["6",".",".","1","9","5",".",".","."],
+#     [".","9","8",".",".",".",".","6","."],
+#     ["8",".",".",".","6",".",".",".","3"],
+#     ["4",".",".","8",".","3",".",".","1"],
+#     ["7",".",".",".","2",".",".",".","6"],
+#     [".","6",".",".",".",".","2","8","."],
+#     [".",".",".","4","1","9",".",".","5"],
+#     [".",".",".",".","8",".",".","7","9"],
+# ]
+# ret = Solution().isValidSudoku(board)
 # print ret
